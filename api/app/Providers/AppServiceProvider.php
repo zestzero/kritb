c05b9a59af1b2e98b91b403f5c94ba57d49b0b52
zestzero <gmkipladen@gmail.com>
2019-12-06 08:09:41 +0700

    add passport

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Fix error
+        // SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long;
+        // max key length is 767 bytes (SQL: alter table users add unique users_email_unique(email))
+        Schema::defaultStringLength(191);
     }
 }
